b4079b83bdf98db8eccc8f8e20998f1189469238
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2025-05-22 21:21:00 +0200

    cleaner code

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = xpress();
 
 app.use(bodyParser.json()).use(cors()).use("/", require("./routes"));
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {

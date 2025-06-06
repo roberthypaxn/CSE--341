ccb1a993099e930594a495c1a15963a7659dc0e4
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2025-06-06 19:27:32 +0200

    Updated Database

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -11,7 +11,7 @@ const initDb = (callback) => {
   }
   MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
-      _db = client.db("Web_Services"); // Get the database instance directly
+      _db = client.db("Web_Movies"); // Get the database instance directly
       callback(null, _db);
     })
     .catch((err) => {
